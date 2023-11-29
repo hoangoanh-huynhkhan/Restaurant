9bbfa80a24b88e92a5398a16a4aa74eadef1d90b
HUYNH VAN CHI KHAN <huynhkhan91@gmail.com>
2023-11-29 14:49:51 +0700

    design from login

--- RestaurantManager/GUI/frm__login.Designer.cs
+++ RestaurantManager/GUI/frm__login.Designer.cs
@@ -30,11 +30,187 @@ namespace GUI
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pic__title = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.btn__close = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.pic__username = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.pic__password = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.txt__username = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txt__password = new Guna.UI2.WinForms.Guna2TextBox();
+            this.btn__login = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.pic__showpassword = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic__title)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn__close)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic__username)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic__password)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn__login)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic__showpassword)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic__title
+            // 
+            this.pic__title.Image = global::GUI.Properties.Resources.login_;
+            this.pic__title.ImageRotate = 0F;
+            this.pic__title.Location = new System.Drawing.Point(144, 62);
+            this.pic__title.Name = "pic__title";
+            this.pic__title.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.pic__title.Size = new System.Drawing.Size(266, 281);
+            this.pic__title.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic__title.TabIndex = 0;
+            this.pic__title.TabStop = false;
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.BorderRadius = 30;
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // btn__close
+            // 
+            this.btn__close.Image = global::GUI.Properties.Resources.closed;
+            this.btn__close.ImageRotate = 0F;
+            this.btn__close.Location = new System.Drawing.Point(12, 12);
+            this.btn__close.Name = "btn__close";
+            this.btn__close.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.btn__close.Size = new System.Drawing.Size(58, 57);
+            this.btn__close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btn__close.TabIndex = 1;
+            this.btn__close.TabStop = false;
+            // 
+            // pic__username
+            // 
+            this.pic__username.Image = global::GUI.Properties.Resources.profile;
+            this.pic__username.ImageRotate = 0F;
+            this.pic__username.Location = new System.Drawing.Point(53, 409);
+            this.pic__username.Name = "pic__username";
+            this.pic__username.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.pic__username.Size = new System.Drawing.Size(58, 57);
+            this.pic__username.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic__username.TabIndex = 2;
+            this.pic__username.TabStop = false;
+            // 
+            // pic__password
+            // 
+            this.pic__password.Image = global::GUI.Properties.Resources.password;
+            this.pic__password.ImageRotate = 0F;
+            this.pic__password.Location = new System.Drawing.Point(53, 522);
+            this.pic__password.Name = "pic__password";
+            this.pic__password.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.pic__password.Size = new System.Drawing.Size(58, 57);
+            this.pic__password.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic__password.TabIndex = 3;
+            this.pic__password.TabStop = false;
+            // 
+            // txt__username
+            // 
+            this.txt__username.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.txt__username.BorderRadius = 15;
+            this.txt__username.BorderThickness = 2;
+            this.txt__username.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt__username.DefaultText = "";
+            this.txt__username.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txt__username.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txt__username.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt__username.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt__username.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt__username.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.txt__username.ForeColor = System.Drawing.Color.Black;
+            this.txt__username.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.txt__username.Location = new System.Drawing.Point(144, 403);
+            this.txt__username.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.txt__username.Name = "txt__username";
+            this.txt__username.PasswordChar = '\0';
+            this.txt__username.PlaceholderText = "Username";
+            this.txt__username.SelectedText = "";
+            this.txt__username.Size = new System.Drawing.Size(344, 70);
+            this.txt__username.TabIndex = 4;
+            // 
+            // txt__password
+            // 
+            this.txt__password.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.txt__password.BorderRadius = 15;
+            this.txt__password.BorderThickness = 2;
+            this.txt__password.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt__password.DefaultText = "";
+            this.txt__password.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txt__password.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txt__password.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt__password.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt__password.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt__password.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.txt__password.ForeColor = System.Drawing.Color.Black;
+            this.txt__password.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.txt__password.Location = new System.Drawing.Point(144, 519);
+            this.txt__password.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.txt__password.Name = "txt__password";
+            this.txt__password.PasswordChar = '\0';
+            this.txt__password.PlaceholderText = "Password";
+            this.txt__password.SelectedText = "";
+            this.txt__password.Size = new System.Drawing.Size(344, 70);
+            this.txt__password.TabIndex = 5;
+            // 
+            // btn__login
+            // 
+            this.btn__login.BorderRadius = 15;
+            this.btn__login.Image = global::GUI.Properties.Resources.ok;
+            this.btn__login.ImageRotate = 0F;
+            this.btn__login.Location = new System.Drawing.Point(189, 622);
+            this.btn__login.Name = "btn__login";
+            this.btn__login.Size = new System.Drawing.Size(174, 88);
+            this.btn__login.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btn__login.TabIndex = 6;
+            this.btn__login.TabStop = false;
+            // 
+            // pic__showpassword
+            // 
+            this.pic__showpassword.BackColor = System.Drawing.Color.White;
+            this.pic__showpassword.Image = global::GUI.Properties.Resources.hide;
+            this.pic__showpassword.ImageRotate = 0F;
+            this.pic__showpassword.Location = new System.Drawing.Point(434, 535);
+            this.pic__showpassword.Name = "pic__showpassword";
+            this.pic__showpassword.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.pic__showpassword.Size = new System.Drawing.Size(43, 40);
+            this.pic__showpassword.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic__showpassword.TabIndex = 7;
+            this.pic__showpassword.TabStop = false;
+            this.pic__showpassword.Visible = false;
+            // 
+            // frm__login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(550, 722);
+            this.Controls.Add(this.pic__showpassword);
+            this.Controls.Add(this.btn__login);
+            this.Controls.Add(this.txt__password);
+            this.Controls.Add(this.txt__username);
+            this.Controls.Add(this.pic__password);
+            this.Controls.Add(this.pic__username);
+            this.Controls.Add(this.btn__close);
+            this.Controls.Add(this.pic__title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm__login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frm__login";
+            ((System.ComponentModel.ISupportInitialize)(this.pic__title)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn__close)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic__username)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic__password)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn__login)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic__showpassword)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2CirclePictureBox pic__title;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox btn__close;
+        private Guna.UI2.WinForms.Guna2TextBox txt__password;
+        private Guna.UI2.WinForms.Guna2TextBox txt__username;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox pic__password;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox pic__username;
+        private Guna.UI2.WinForms.Guna2PictureBox btn__login;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox pic__showpassword;
     }
 }
